78c1b2facd948dd2e6ccec43616963d6d745f879
jrdrg <jrodriguez.0987+github@gmail.com>
2021-05-24 16:59:35 -0400

    add build script and dist directory

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  testPathIgnorePatterns: ["/node_modules/", "/__fixtures__/"]
+  testPathIgnorePatterns: ["/node_modules/", "/__fixtures__/", "dist"]
 };
